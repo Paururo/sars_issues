ba113794161b93db6841a946e8a0310ade1ef4cd
Paula Ruiz-Rodriguez <50167687+Paururo@users.noreply.github.com>
2024-01-05 15:59:57 +0100

    add all code in functions

--- sars_lineages.py
+++ sars_lineages.py
@@ -2,36 +2,44 @@ import json
 import subprocess
 import datetime
 
-# Get the current date and time
-now = datetime.datetime.now()
-# Format the date and time as a string
-date_string = now.strftime("%Y-%m-%d")
-output_file = f"pangolist_{date_string}.txt"
-output_file2 = f"voclist_{date_string}.txt"
+def get_current_date_string():
+  now = datetime.datetime.now()
+  return now.strftime("%Y-%m-%d")
 
-# Download the JSON file from GitHub
-subprocess.call(['wget', 'https://github.com/cov-lineages/lineages-website/raw/master/_data/lineage_data.full.json'])
+def download_lineage_data():
+  subprocess.call(['wget', 'https://github.com/cov-lineages/lineages-website/raw/master/_data/lineage_data.full.json'])
 
-# Open the JSON file
-with open('lineage_data.full.json', 'r') as f:
-  data = json.load(f)
+def remove_lineage_data():
+  subprocess.call(['rm', 'lineage_data.full.json'])
 
-# Define lineage mapping
-lineage_mapping = {
-  "B.1.1.529.": "Omicron",
-  "B.1.617.2.": "Delta",
-  "B.1.1.7.": "Alpha",
-  "B.1.177.": "20E"
-}
+def process_lineage_data(lineage_data):
+  output_file = f"pangolist_{get_current_date_string()}.txt"
+  output_file2 = f"voclist_{get_current_date_string()}.txt"
 
-# Process data and write to output files
-with open(output_file, 'w') as out_file, open(output_file2, 'w') as out_file2:
-  for key, value in data.items():
-    lineage = key.split(':')[0]
-    description = value.get('Description', 'No description available')
-    out_file.write(f"{lineage}: {description}\n")
-    if lineage in lineage_mapping:
-      out_file2.write(f"{lineage}: {lineage_mapping[lineage]}\n")
+  lineage_mapping = {
+    "B.1.1.529.": "Omicron",
+    "B.1.617.2.": "Delta",
+    "B.1.1.7.": "Alpha",
+    "B.1.177.": "20E"
+  }
 
-# Remove the downloaded JSON file
-subprocess.call(['rm', 'lineage_data.full.json'])
+  with open(output_file, 'w') as out_file, open(output_file2, 'w') as out_file2:
+    for key, value in lineage_data.items():
+      lineage = key.split(':')[0]
+      description = value.get('Description', 'No description available')
+      out_file.write(f"{lineage}: {description}\n")
+      if lineage in lineage_mapping:
+        out_file2.write(f"{lineage}: {lineage_mapping[lineage]}\n")
+
+def main():
+  download_lineage_data()
+
+  with open('lineage_data.full.json', 'r') as f:
+    data = json.load(f)
+
+  process_lineage_data(data)
+
+  remove_lineage_data()
+
+if __name__ == "__main__":
+  main()
